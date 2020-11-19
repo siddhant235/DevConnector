0f5da6470a1fcb82ce1be97b75574233823f4ce8
siddhant235 <siddhant235@gmail.com>
2020-11-19 23:06:38 +0530

    auth backend completed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express=require("express");
 const router =express.Router();
 const auth=require("../../middleware/auth") 
+const{check,validationResult}=require("express-validator")
 const User=require("../../modals/User")
+const jwt = require("jsonwebtoken");
+const config=require("config")
+const bcrypt=require('bcryptjs')
 //@route GET api/auth
 
 
@@ -23,4 +27,50 @@ router.get('/',auth,async (req,res)=>
 }
 )
 
+//authenticate user and get token
+router.post('/',[
+    
+  check('email','Please include a valid email').isEmail(),
+  check('password','Please enter a password with 6 or more characters').exists()
+],async (req,res)=>{
+  
+  const errors=validationResult(req);
+  if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()})
+  }
+const {email,password}=req.body
+
+try{
+
+let user=await User.findOne({email});
+if(!user)
+{
+ return res.status(400).json({errors:[{msg:'Invalid credentials'}]});
+}
+
+const isMatch=await bcrypt.compare(password,user.password)
+
+if(!isMatch)
+{
+  return res.status(400).json({errors:[{msg:'Invalid Credentials'}]})
+}
+const payload={
+   user:{
+       id:user.id
+   }
+}
+jwt.sign(payload,config.get('jwtSecret'),{expiresIn:36000},(err,token)=>{
+ if(err)
+ throw err;
+ res.json({token});  
+
+});
+}catch(err){
+console.log(err.message);
+res.status(500).send('Server Error');
+}
+  
+ }
+  
+  )
 module.exports=router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,27 @@
 const express=require("express");
 const router =express.Router();
+const auth=require("../../middleware/auth")
+const User=require("../../modals/User")
+const Profile=require("../../modals/Profile")
  //@route GET api/users
  //@desc Test route
  //@access Public
-router.get('/',(req,res)=>res.send('Profile Route'))
+router.get('/me',auth,async (req,res)=>
+{
+    try{
+     const profile=await Profile.findOne({user:req.user.id}).populate('user',['name','avatar']);
+if(!profile)
+{
+    return res.status(400).json({msg:"There is no Profile"})
+}
+res.json(profile)
+    }
+    catch(err){
+          console.error(err.message)
+          res.status(500).send("Internal Server Error");
+    }
+}
+
+)
 
 module.exports=router;
